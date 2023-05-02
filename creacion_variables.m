731af4213f9165652ad0c292020cbf6d475a1dea
dazaironther <132408824+dazaironther@users.noreply.github.com>
2023-05-02 17:13:38 -0600

    operaciones basicas

---
+++ creacion_variables.m
@@ -0,0 +1,26 @@
+clear
+clear all
+close all
+
+x = 3.1;
+y = -2.5;
+% estas son las operaciones basicas
+suma = x + y;
+resta = x - y;
+multiplicacion = x * y; %multiplicacion
+divicion = x / y;
+potencia = x^y;
+logaritmo_natural = log(x);
+log_10 = log10(x);
+exponencial = exp(y);
+cos(x);  % este coseno no los da en radianes
+cosd(x); % nos da el anguno coseno en grados
+sin(x); % seno del angulo
+sind(x); %seno del angulo
+%% operacines con numeros complejos
+x = 3 - 2i;  %numero complejo
+y = -2.1 +3.5i; % numero complejo
+ x + y  ;% suma de numeros complejos 
+ abs(x);% magnitud o modulo de numero complejo
+ magnitud = sqrt(real(x)^2 + imag(x)^2);
+ angule = angle(x); % angulo del numero complejo
